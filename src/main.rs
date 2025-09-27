4af206202813de1ff0aeebdc9391b0027ea3f088
“subhajit <subhajitchaudhury05@gmail.com>
2025-09-28 04:40:13 +0530

    skeleton to return order id

--- src/main.rs
+++ src/main.rs
@@ -42,10 +42,27 @@ enum Side {
     Sell,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct createOrderResponse {
+    order_id: String,
+}
+
 #[post("/order")]
 async fn create_order(body: Json<CreateOrderInput>) -> impl Responder {
     println!("Create order: {:?}", body);
-    "create order"
+
+    // get the data
+    let price = body.0.price;
+    let qty = body.0.qty;
+    let user_id = body.0.user_id;
+    let side = &body.0.side;
+
+    // do whatever the fuck you want i don't care
+
+    // maintain orderbook logic
+    HttpResponse::Ok().json(createOrderResponse {
+        order_id: String::from("hello"),
+    })
 }
 
 #[delete("/order")]
